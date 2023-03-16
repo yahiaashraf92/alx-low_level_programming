aee47b2c56f216c1b9657fb755799d82a9a686f0
yahiashraf <yahiahamaid@gmail.com>
2023-03-16 10:28:46 -0700

    loll

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
-int main(){
+int main(void)
+{
+	/*
+	 * comment
+	 */
+	char str[] = "\"Programming is like building a multilingual puzzle";
 
-	    char str[] = "\"Programming is like building a multilingual puzzle";
-	        puts(str);
-
-		    return 0;
+	puts(str);
+	return (0);
 }
