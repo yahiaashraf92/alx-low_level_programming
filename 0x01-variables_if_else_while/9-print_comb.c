58dd96e51cfa03a010a12a4b45555f5dd3b9e154
yahiashraf <yahiahamaid@gmail.com>
2023-03-17 06:23:58 -0700

    fds

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+/* more headers goes there */
+
+/**
+ *  main - Entry point
+ *  Description: prints a message that depends on
+ *  last digit of the random number generated
+ *  Return: Always 0 Success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		putchar(i + '0');
+		putchar(' ');
+	}
+	putchar('9');
+	putchar('\n');
+
+	return (0);
+}
