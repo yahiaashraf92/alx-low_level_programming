f13e9f96ec340e162846d2cd99fc4be60c79e5e4
yahiashraf <yahiahamaid@gmail.com>
2023-03-17 06:07:31 -0700

    fds

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+/* more headers goes there */
+
+/**
+ *  main - Entry point
+ *  Description: prints a message that depends on
+ *  last digit of the random number generated
+ *  Return: Always 0 Success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
