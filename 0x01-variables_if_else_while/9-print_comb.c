08708b28384ffc1b50486a9eef0519b8796728b6
yahiashraf <yahiahamaid@gmail.com>
2023-03-17 06:26:47 -0700

    fds

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,13 +13,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
+		if (i == 9)
+			continue;
 		putchar(',');
 		putchar(' ');
 	}
-	putchar('9');
 	putchar('\n');
 
 	return (0);
