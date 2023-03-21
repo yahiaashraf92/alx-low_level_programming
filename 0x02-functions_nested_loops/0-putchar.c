3a4d2b8716482300282e6da3e8705c13a34f81e5
yahiashraf <yahiahamaid@gmail.com>
2023-03-21 10:04:14 -0700

    sad

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ * Description: prints a message that depends on
+ * last digit of the random number generated
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+
+	printf("_putchar\n");
+
+	return (0);
+}
