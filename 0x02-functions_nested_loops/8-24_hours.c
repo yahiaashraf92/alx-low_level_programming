9a0c0f4868d3551600fd21cbf3b686c6b5a9342c
yahiashraf <yahiahamaid@gmail.com>
2023-03-21 11:59:58 -0700

    sad

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,16 +7,29 @@
  */
 void jack_bauer(void)
 {
-	int h, m;
+	int h1, h2, m1, m2;
 
-	for (h = 0; h < 24; h++)
+	for (h1 = 0; h1 < 2; h1++)
 	{
-		for (m = 0; m < 60; m++)
+		for (h2 = 0; h2 < 10; h2++)
 		{
-			_putchar(h + '0');
-			_putchar(':');
-			_putchar(m + '0');
-			_putchar('\n');
+			if (h1 == 2 && h2 > 3)
+			{
+				break;
+			}
+			for (m1 = 0; m1 <= 5; m1++)
+			{
+				for (m2 = 0; m2 < 10; m2++)
+				{
+					_putchar(h1 + '0');
+					_putchar(h2 + '0');
+					_putchar(':');
+					_putchar(m1 + '0');
+					_putchar(m2 + '0');
+					_putchar('\n');
+				}
+			}
 		}
 	}
+
 }
