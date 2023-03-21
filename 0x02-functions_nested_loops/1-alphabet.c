6aef935c6dfa927091b188065e69f82a60a2fdbb
yahiashraf <yahiahamaid@gmail.com>
2023-03-21 10:21:13 -0700

    sad

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ * Description: prints a message that depends on
+ * last digit of the random number generated
+ * Return: Always 0 Success
+ */
+
+
+void print_alphabet()
+{
+	int i;
+	for (i = 'a'; i<= 'z'; i++)
+	{
+		putchar(i);
+	}
+	_putchar('\n');
+}
