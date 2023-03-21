536dc2ff029786b68d7913ef94be439e7aca8ada
yahiashraf <yahiahamaid@gmail.com>
2023-03-21 10:38:02 -0700

    sad

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - void parameters
+ * Description: prints a message that depends on
+ * last digit of the random number generated
+ * Return: Always void Success
+ */
+void print_alphabet(void)
+{
+	int j, i;
+
+	for (j = 0; j < 10; j++)
+	{
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	}
+	_putchar('\n');
+}
