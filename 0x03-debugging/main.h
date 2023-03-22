94865125dd3341e3f19638d4b727ca92e321dce3
yahiashraf <yahiahamaid@gmail.com>
2023-03-22 11:40:09 -0700

    didi

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,3 @@
-8 lines (7 sloc)  194 Bytes
-
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
@@ -8,3 +6,4 @@ void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int, int, int);
 int convert_day(int, int);
+#endif /* MAIN_H */
