d37e3899681a3b876067517332abf33ae802be30
yahiashraf <yahiahamaid@gmail.com>
2023-03-23 08:09:58 -0700

    habiby

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * more_numbers - printt
+ * return : void
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 63; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
