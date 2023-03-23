e3eaaf6185a8e94c034107f2a9520ebea061d5d1
yahiashraf <yahiahamaid@gmail.com>
2023-03-23 08:24:24 -0700

    habiby

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,11 @@ void more_numbers(void)
 
 	for (j = 0; j < 10; j++)
 	{
-	for (i = 48; i < 63; i++)
+	for (i = 0; i < 15; i++)
 	{
-		_putchar(i);
+		if (i > 9)
+			_putchar('1');
+		_putchar(i % 10 + '0');
 	}
 	_putchar('\n');
 	}
