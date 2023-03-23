1b312f01e9d926db08de6ff0ac42615bca690939
yahiashraf <yahiahamaid@gmail.com>
2023-03-23 07:38:50 -0700

    habiby

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+/*
+ * supper - checks if character is upper alphabet
+ * Return: 1 if true 0 if false
+ * @c: character given to get checked
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
