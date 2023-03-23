9688672a2651a12e7a6a7cbcbbea1a1b1ecb5ea7
yahiashraf <yahiahamaid@gmail.com>
2023-03-23 08:02:49 -0700

    habiby

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - printt
+ * return : void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
