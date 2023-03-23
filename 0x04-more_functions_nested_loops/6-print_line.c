2ad72a9d5f962268199728bcd55eaa1988433dc1
yahiashraf <yahiahamaid@gmail.com>
2023-03-23 08:37:21 -0700

    habiby

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
- * print_line - printt
- * return : void
- * @n : number
+ * print_line - prints line
+ * Return: void
+ * @n: number of lines
  */
+
 void print_line(int n)
 {
 	int i;
 
 	for (i = 0; i < n; i++)
-	{
 		_putchar('_');
-	}
-	_putchar('$');
+
 	_putchar('\n');
 }
