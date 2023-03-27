da4045ed8e7137d528b406d669222d277832e6e6
yahiashraf <yahiahamaid@gmail.com>
2023-03-27 08:28:39 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * rev_string - returns the length of a string
+ * @s: string
+ * Return: the length of the string passed as input
+ */
+void rev_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+
+	i--;
+
+	char *temp[i];
+
+	strcpy(temp, s);
+
+	for (j = 0; j < i; j++)
+	{
+		s[j] = temp[i - j];
+	}
+}
