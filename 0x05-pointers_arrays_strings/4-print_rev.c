7ade86e2672f6eacc94cf5c330f18961fe7b14d0
yahiashraf <yahiahamaid@gmail.com>
2023-03-27 07:58:52 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - returns the length of a string
+ * @s: string
+ * Return: the length of the string passed as input
+ */
+int print_rev(char *s)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+
+
+	return (i);
+}
