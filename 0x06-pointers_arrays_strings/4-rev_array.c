44af7a601469ebb96ada475f3ae67be7b94a5575
yahiashraf <yahiahamaid@gmail.com>
2023-03-29 10:07:26 -0700

    more pointers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "main.h"
 /**
  * reverse_array - returns the length of a string
  * @a: array
  * @n: string
  * Return: void
  */
+
 void reverse_array(int *a, int n)
 {
-	int i, j;
+	int i, temp;
 
-	int temp[100];
 
-	for (i = 0; i < n; i++)
+	for(i = 0; i < n / 2; i++)
 	{
-		temp[i] = a[i];
-	}
-
-	for (i = 0; i < n; i++)
-	{
-		a[i] = temp[n - i - 1];
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
 	}
 
 }
