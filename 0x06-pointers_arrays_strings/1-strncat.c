a31b3a58c6b1ab960a148179c5c83970251f078a
yahiashraf <yahiahamaid@gmail.com>
2023-03-29 08:09:34 -0700

    more pointers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,9 +4,10 @@
  *_strcat - concatenates two strings
  * @dest: string to append to
  * @src: string to add
+ * @n: int num
  * Return: a pointer to the resulting string
  */
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src, int n)
 {
 	int i, j;
 
