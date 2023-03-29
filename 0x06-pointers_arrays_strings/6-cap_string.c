7275f762d4b8aa0cc9a824f716120c757dc7bfa9
yahiashraf <yahiahamaid@gmail.com>
2023-03-29 11:49:13 -0700

    more pointers

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,31 +1,34 @@
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdlib.h>
-/**
- * cap_string - returns the length of a string
- * @s: char array
- * Return: char
- */
+#include "main.h"
 
+/**
+ * cap_string - capitalizes everey word of a string
+ * @s: string to modify
+ * Return: the resulting string
+ */
 char *cap_string(char *s)
 {
-	int i;
+	int i, j;
 
-	i = 0;
 
-	while (s[i] != '\0')
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i - 1] == ' ' || s[i - 1] == ';'
-		|| s[i - 1] == '.' || s[i - 1] == '!' ||
-		s[i - 1] == '?' || s[i - 1] == '('
-		|| s[i - 1] == ')' || s[i - 1] == '{'
-		|| s[i - 1] == '}' || s[i - 1] == '\n'
-		|| s[i - 1] == ',' || s[i - 1] == '\t')
-			s[i] = toupper(s[i]);
-		i++;
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == spe[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
 	}
 
 	return (s);
 }
-
