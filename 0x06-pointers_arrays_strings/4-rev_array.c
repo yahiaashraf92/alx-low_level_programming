2b471a30ff03b3bd05d76c2fa49ceb8f46cd6fad
yahiashraf <yahiahamaid@gmail.com>
2023-03-29 10:08:44 -0700

    more pointers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,14 +10,15 @@
 
 void reverse_array(int *a, int n)
 {
-	int i, temp;
+	int i, j, temp;
 
+	j = n - 1;
 
 	for(i = 0; i < n / 2; i++)
 	{
 		temp = a[i];
-		a[i] = a[n - i - 1];
-		a[n - i - 1] = temp;
+		a[i] = a[j];
+		a[j--] = temp;
 	}
 
 }
