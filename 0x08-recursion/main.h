31a94e35bcf39ac6aaf243eb2d64635dd2950e22
yahiashraf <yahiahamaid@gmail.com>
2023-04-05 08:15:54 -0700

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,8 +1,5 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
-
-int _putchar(char c);
 void _puts_recursion(char *s);
+int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
@@ -11,5 +8,3 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-
-#endif
