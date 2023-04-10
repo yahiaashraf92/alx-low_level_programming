63ae4fa5bc85c7897872a606fd9c34a742fd98c1
yahiaashraf92 <yahiahamaid@gmail.com>
2023-04-10 08:59:51 -0700

    static

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_atoi - lol
+ *@s : string
+ *Return: 0 or 1
+ */
+int _atoi(char *s)
+{
+	return (1);
+}
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_putchar - lol
+ * @c : char
+ * Return: 0 or 1
+*/
+int _putchar(char c)
+{
+	return (1);
+}
