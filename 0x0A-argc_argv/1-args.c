2ad1ef98759483015e9630f568b12afeee397a3b
yahiaashraf92 <yahiahamaid@gmail.com>
2023-04-10 09:12:55 -0700

    arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the program name followed by a newline
+ * @argc: number of arguments
+ * @argv: array of arguments given
+ * Return: 0 Always
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc-1);
+
+	return (0);
+}
