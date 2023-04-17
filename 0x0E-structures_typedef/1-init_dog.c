bba74cd4c0ab4b8f350da5734cc5991b3d76f504
yahiaashraf92 <yahiahamaid@gmail.com>
2023-04-17 06:57:08 -0700

    typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,12 +8,11 @@
  * @age: age to initialize
  * @owner: owner to initialize
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		d = malloc(sizeof(struct dog);
+		d = malloc(sizeof(struct dog));
 	d->name = name;
 	d->age = age;
-	d->owner = owner
+	d->owner = owner;
 }
