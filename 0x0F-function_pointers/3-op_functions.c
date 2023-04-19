947c199d1cf1634c462fb99cd9071b717d000266
yahiaashraf92 <yahiahamaid@gmail.com>
2023-04-19 08:46:44 -0700

    pointers;git push

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,7 +1,7 @@
 #include "3-calc.h"
 
 /**
- * cop_add - calculates the sum of two integers
+ * op_add - calculates the sum of two integers
  * @a: first integer
  * @b: second integer
  * Return: sum of a and b
